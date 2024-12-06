d1d42234f2a57ac1e5fe82f090360ea404461421
Matt Carreon <matt.carreon@workday.com>
2024-12-06 14:44:33 -0800

    part 2 wont work why

--- python/6/part_2.py
+++ python/6/part_2.py
@@ -1,7 +1,7 @@
 import copy
 
-# input_file = '/home/mattc/aoc2/python/6/input.txt'
-input_file = '/home/mattc/aoc2/python/6/example.txt'
+# input_file = 'input.txt'
+input_file = 'example.txt'
 
 directions = {
     '^': (-1, 0),
@@ -26,67 +26,85 @@ def find_guard_status(lab_map: list) -> dict:
             if lab_map[row][col] in directions:
                 return { 'coordinates': [row, col], 'facingDirection': lab_map[row][col] }
             
-def check_cycle(start_coordinates: list, direction: str, lab_map: list, ans: list, visited: set) -> None:
+def is_inbounds(coordinates: list, row_len: int, col_len: int) -> bool:
+    return coordinates[0] >= 0 and coordinates[0] < row_len and coordinates[1] >= 0 and coordinates[1] < col_len
+
+def is_cycle(start_coordinates: list, direction: str, lab_map: list, visited: set) -> None:
+    print('looking for cycle')
     stack = [(start_coordinates, turn_right(direction), start_coordinates)]
-    print(lab_map)
     row_len = len(lab_map)
     col_len = len(lab_map[0])
-    cycle_check = set()
+
     while stack:
         coordinates, direction, old_coordinates = stack.pop()
-        if coordinates[0] < 0 or coordinates[0] >= row_len or coordinates[1] < 0 or coordinates[1] >= col_len:
+        vector = tuple((tuple(coordinates), direction))
+
+        if not is_inbounds(coordinates, row_len, col_len):
             continue
         
-        if lab_map[coordinates[0]][coordinates[1]] == '#':
+        if lab_map[coordinates[0]][coordinates[1]] == '#' or lab_map[coordinates[0]][coordinates[1]] == 'O':
             new_direction = turn_right(direction)
             stack.append((old_coordinates, new_direction, coordinates))
             continue
-
-        if (tuple(coordinates), direction) in cycle_check or (tuple(coordinates), direction) in visited:
-            print('Found cycle at', coordinates, direction)
-            ans[0] += 1
-            return
         
+        if vector in visited:
+            pretty_print_lab_map(lab_map)
+            print('Cycle detected!')
+            return 1
+
         if lab_map[coordinates[0]][coordinates[1]] == '.' or lab_map[coordinates[0]][coordinates[1]] in directions:
-            cycle_check.add(tuple((tuple(coordinates), direction)))
+            visited.add(vector)
+            lab_map[coordinates[0]][coordinates[1]] = 'X'
 
         next_coordinates = [sum(x) for x in zip(coordinates, directions[direction])]
 
         stack.append((next_coordinates, direction, coordinates))
 
-def dfs(start_coordinates: list, direction: str, lab_map: list, ans: list) -> None:
+    return 0
+
+def pretty_print_lab_map(lab_map):
+    for row in lab_map:
+        print(' '.join(row))
+    print()  # Add an empty line for better readability
+
+def dfs(start_coordinates: list, direction: str, lab_map: list) -> None:
     stack = [(start_coordinates, direction, start_coordinates)]
     row_len = len(lab_map)
     col_len = len(lab_map[0])
+    cycle_count = 0
     visited = set()
 
     while stack:
         coordinates, direction, old_coordinates = stack.pop()
+        vector = tuple((tuple(coordinates), direction))
+
         if coordinates[0] < 0 or coordinates[0] >= row_len or coordinates[1] < 0 or coordinates[1] >= col_len:
             continue
         
-        if lab_map[coordinates[0]][coordinates[1]] == '#':
+        if lab_map[coordinates[0]][coordinates[1]] == '#' or lab_map[coordinates[0]][coordinates[1]] == 'O':
             new_direction = turn_right(direction)
             stack.append((old_coordinates, new_direction, coordinates))
             continue
 
         if lab_map[coordinates[0]][coordinates[1]] == '.' or lab_map[coordinates[0]][coordinates[1]] in directions:
-            visited.add(tuple((tuple(coordinates), direction)))
+            visited.add(vector)
+            lab_map[coordinates[0]][coordinates[1]] = 'X'
 
         next_coordinates = [sum(x) for x in zip(coordinates, directions[direction])]
 
-        if not(next_coordinates[0] < 0 or next_coordinates[0] >= row_len or next_coordinates[1] < 0 or next_coordinates[1] >= col_len):
-            print("Placing blockade at next coordinates: ", next_coordinates)
-            copy_lab_map = copy.deepcopy(lab_map)
-            copy_lab_map[next_coordinates[0]][next_coordinates[1]] = '#'
-            check_cycle(coordinates, direction, copy_lab_map, ans, visited)
+        if is_inbounds(next_coordinates, row_len, col_len) and lab_map[next_coordinates[0]][next_coordinates[1]] != '#':
+            lab_map_copy = copy.deepcopy(lab_map)
+            lab_map_copy[next_coordinates[0]][next_coordinates[1]] = 'O'
+            pretty_print_lab_map(lab_map_copy)
+            visited_copy = set(visited)
+            cycle_count += is_cycle(coordinates, direction, lab_map_copy, visited_copy)
 
         stack.append((next_coordinates, direction, coordinates))
+    
+    return cycle_count
 
 with open(input_file, 'r') as file:
     lab_map = [list(line) for line in file.read().splitlines()]
 
 guard_status = find_guard_status(lab_map)
-ans = [0]
-dfs((guard_status['coordinates']), guard_status['facingDirection'], lab_map, ans)
-print(ans)
+print(dfs((guard_status['coordinates']), guard_status['facingDirection'], lab_map))
